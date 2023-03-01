0fccd303aa29deea758b786c874a4e366e6f72b3
Janusz Piechówka <januszpiechowka@gmail.com>
2023-03-01 11:30:09 +0100

    Add code to fetch IP address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+const API_URL: &str = "https://api.ipify.org";
+
+fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::blocking::get(API_URL)?;
+    let body = res.text()?;
+    println!("{body}");
+    Ok(())
 }
